f049166cb50a36c87d10065dbdb49a700a7d8c4b
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-11 14:14:51 +0530

    chore: Remove file_hash col from entities table

--- src/deputy/database/sqlite/schema.sql
+++ src/deputy/database/sqlite/schema.sql
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS branch_files (
 
 CREATE TABLE IF NOT EXISTS entities (
     id            TEXT PRIMARY KEY,
-    file_hash     TEXT NOT NULL,
     language      TEXT NOT NULL,
     full_path     TEXT NOT NULL,
     name          TEXT NOT NULL,
@@ -16,7 +15,6 @@ CREATE TABLE IF NOT EXISTS entities (
     metadata_json TEXT NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_entities_file_hash ON entities(file_hash);
 CREATE INDEX IF NOT EXISTS idx_entities_name ON entities(name);
 CREATE INDEX IF NOT EXISTS idx_entities_full_path ON entities(full_path);
 
