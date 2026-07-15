cd48e6e845a165e0b0f2bf207a04a81e284ee2e2
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-16 00:06:04 +0530

    feat: Make caching opt-in

--- src/deputy/core.py
+++ src/deputy/core.py
@@ -8,16 +8,16 @@ from .database.sqlite import (
     SqlitePythonResolver
 )
 
-def create_context(base_path: str, conn) -> Context:
+def create_context(base_path: str, conn, enable_cache: bool = False) -> Context:
     ctx = Context(base_path=base_path)
     ctx.set_language("python", [".py", ".pyi"], aliases=["py"])
-    ctx.set_skip_paths({".venv", ".git", "__pycache__", ".mypy_cache", ".pytest_cache", ".hg", ".svn"})
     ctx.set_parser("python", PythonSourceParser())
     ctx.set_resolver("python", SqlitePythonResolver(conn))
     ctx.set_linker("python", PythonLinker())
-    ctx.set_symbol_cache(SqliteSymbolCache(conn))
     ctx.set_skip_paths({
         "*.egg-info", "*.dist-info", "__pycache__", "node_modules", ".git",
         ".venv", ".mypy_cache", ".pytest_cache", "build", "dist",
     })
+    if enable_cache:
+        ctx.set_symbol_cache(SqliteSymbolCache(conn))
     return ctx
