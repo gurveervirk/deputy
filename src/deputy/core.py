04c892fe9d586b0a36fad29bdc17e27c39ebb42b
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-25 23:41:27 +0530

    chore: Remove demised resolver

--- src/deputy/core.py
+++ src/deputy/core.py
@@ -5,14 +5,12 @@ from deproc.plugins.python import (
 )
 from .database.sqlite import (
     SqliteSymbolCache,
-    SqlitePythonResolver
 )
 
 def create_context(base_path: str, conn, enable_cache: bool = False) -> Context:
     ctx = Context(base_path=base_path)
     ctx.set_language("python", [".py", ".pyi"], aliases=["py"])
     ctx.set_parser("python", PythonSourceParser())
-    ctx.set_resolver("python", SqlitePythonResolver(conn))
     ctx.set_linker("python", PythonLinker())
     ctx.set_skip_paths({
         "*.egg-info", "*.dist-info", "__pycache__", "node_modules", ".git",
