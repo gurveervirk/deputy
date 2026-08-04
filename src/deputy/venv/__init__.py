8187b10d059e5b4dfbfae48894fc535c32186467
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-08-04 23:38:15 +0530

    chore: Move venv utils to deproc and add new utils dependency

--- src/deputy/venv/__init__.py
+++ src/deputy/venv/__init__.py
@@ -1,12 +1,5 @@
-from .detect import detect_venv, parse_pyvenv_cfg
-from .discovery import find_site_packages, list_installed_packages, PackageInfo
 from .process import process_dependency
 
 __all__ = [
-    "detect_venv",
-    "parse_pyvenv_cfg",
-    "find_site_packages",
-    "list_installed_packages",
-    "PackageInfo",
     "process_dependency",
 ]
